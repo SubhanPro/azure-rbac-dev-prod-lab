4ba8e966b7354d4709e54ce0e4fce4f7f6b17ee1
SubhanPro <72865360+SubhanPro@users.noreply.github.com>
2026-07-17 23:25:20 +0400

    Add Azure deployment script for resource groups and storage
    This script creates Azure resource groups and storage accounts for QA and Staging environments, and verifies their creation.

--- powershell/deploy.ps1
+++ powershell/deploy.ps1
@@ -1,1 +1,39 @@
+# ==========================================
+# Azure PowerShell Deployment Script
+# Project: Azure RBAC DEV/QA/STAGING Lab
+# ==========================================
 
+# Create Resource Groups
+
+New-AzResourceGroup `
+    -Name OCEAN-QA `
+    -Location norwayeast
+
+New-AzResourceGroup `
+    -Name OCEAN-STAGING `
+    -Location norwayeast
+
+
+# Verify Resource Groups
+
+Get-AzResourceGroup
+
+
+# Create Storage Accounts
+
+New-AzStorageAccount `
+    -Name oceanqastorage888 `
+    -ResourceGroupName OCEAN-QA `
+    -Location norwayeast `
+    -SkuName Standard_LRS
+
+New-AzStorageAccount `
+    -Name oceanstgstorage888 `
+    -ResourceGroupName OCEAN-STAGING `
+    -Location norwayeast `
+    -SkuName Standard_LRS
+
+
+# Verify Storage Accounts
+
+Get-AzStorageAccount
